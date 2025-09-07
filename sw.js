9438f4d61caae670be6ba2083850df6251c7757e
rachidoutgadirt-byte <rachid.outgadirt@gmail.com>
2025-09-07 13:59:57 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'placement-assessment-app';
+const URLS_TO_CACHE = [
+  '/', // يمثل الصفحة الرئيسية
+  '/index.html' // اسم ملف HTML الرئيسي الخاص بك
+];
+
+// عند التثبيت، قم بتخزين الملفات الأساسية
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(URLS_TO_CACHE);
+      })
+  );
+});
+
+// عند طلب أي ملف، حاول إحضاره من الشبكة أولاً، وإذا فشل، أحضره من الكاش
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
